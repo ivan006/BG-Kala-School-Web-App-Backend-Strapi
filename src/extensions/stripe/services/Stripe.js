215ae2c758db79c75960b943da5743a156a35cc8
ivan006 <ivan.copeland2015@gmail.com>
2023-05-27 14:08:16 +0200

    1

--- src/extensions/stripe/services/Stripe.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  /**
-   * This method can be used to handle the webhook event for payment failure.
-   * Implement your logic here to unlink the user from the privileged role.
-   */
-  handlePaymentFailure: async (payload) => {
-    // Implement your logic to handle payment failures
-  },
-};
