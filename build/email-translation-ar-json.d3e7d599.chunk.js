3b7f50ffbbaadd6fb7f22798abeb4597f53497cc
ivan006 <ivan.copeland2015@gmail.com>
2023-06-24 20:01:31 +0200

    main

--- build/email-translation-ar-json.d3e7d599.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbg_kala_school_web_app_backend=self.webpackChunkbg_kala_school_web_app_backend||[]).push([[2648],{96551:a=>{a.exports={}}}]);
--- build/email-translation-ru-json.66776543.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbg_kala_school_web_app_backend=self.webpackChunkbg_kala_school_web_app_backend||[]).push([[5388],{52553:a=>{a.exports={}}}]);
